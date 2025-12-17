7aeb8d324813e421ebc3f74b60f31b387d2a7b66
Avinash Thummala <avinash.thummala@ucovyconnects.com>
2025-12-17 01:26:36 -0600

    Base fixture

--- fixtures/baseFixture.js
+++ fixtures/baseFixture.js
@@ -1,26 +1,31 @@
 import { test as base, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage/LoginPage.js";
 import { TestDataReader } from "../utils/TestDataReader.js";
+
 /**
-* @typedef {Object} BaseFixture
-* @property {LoginPage} loginPage
-* @property {import('@playwright/test').BrowserContext} authenticatedContext
-* @property {import('@playwright/test').Page} authenticatedPage
-* @property {Record<string, any>} testData
-*/
+ * @typedef {Object} BaseFixture
+ * @property {LoginPage} loginPage
+ * @property {import('@playwright/test').BrowserContext} authenticatedContext
+ * @property {import('@playwright/test').Page} authenticatedPage
+ * @property {Record<string, any>} testData
+ */
+
 export const baseBest = base.extend({
- authenticatedContext: async ({ browser }, use) => {
-   const context = await browser.newContext();
-   await use(context);
-   await context.close();
- },
- authenticatedPage: async ({ authenticatedContext }, use) => {
-   const page = await authenticatedContext.newPage();
-   await page.goto('/');
-   await use(page); // provide the page to tests
- },
- testData: async ({ }, use, testInfo) => {
-   const reader = new TestDataReader(testInfo.file, "data");
-   await use(reader.testData);
- }
+  authenticatedContext: async ({ browser }, use) => {
+    const context = await browser.newContext();
+    await use(context);
+    await context.close();
+  },
+  
+  authenticatedPage: async ({ authenticatedContext }, use) => {
+    const page = await authenticatedContext.newPage();
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await use(page);
+  },
+  
+  testData: async ({ }, use, testInfo) => {
+    const reader = new TestDataReader(testInfo.file, "data");
+    await use(reader.testData);
+  }
 });
