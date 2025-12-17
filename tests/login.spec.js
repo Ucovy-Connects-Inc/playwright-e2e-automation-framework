fee68028390f70ec628c6f2ade12a1995a0f67cd
Avinash Thummala <avinash.thummala@ucovyconnects.com>
2025-12-17 02:40:17 -0600

    Final fix for git Actions

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -6,7 +6,7 @@ import { CheckpointManager } from "../utils/CheckpointManager.js";
  
 test.describe("Login Page Tests", () => {
  
-  test("should show error message for invalid credentials @smoke @negative", async ({ testData, authenticatedPage }) => {
+  test("should show error message for invalid credentials @smoke @Loginnegative", async ({ testData, authenticatedPage }) => {
     const { login, registration } = testData;
     const checkpointManager = new CheckpointManager();
     const loginPage = new LoginPage(authenticatedPage);
@@ -22,4 +22,28 @@ test.describe("Login Page Tests", () => {
     await loginPage.clickLogin();
     checkpointManager.assertAllCheckpoints();
   });
+
+  test("Login with valid credentials @smoke @positive", async ({ authenticatedPage, testData }) => {
+    const { login } = testData;
+    const loginPage = new LoginPage(authenticatedPage);
+    await loginPage.login(login.ValidUsername, login.ValidPassword);
+    expect(await loginPage.verifyLoginSuccess()).toBeTruthy();
+
+  });
+
+  test("Validate Links on Login Page @regression", async ({ authenticatedPage }) => {
+    const checkpointManager = new CheckpointManager();
+    const loginPage = new LoginPage(authenticatedPage);
+    await checkpointManager.createCheckpoint("Validate Logo Visibility on Login Page", await loginPage.isLogoVisibleOnLoginPage(), true, "Logo should be visible on Login Page");
+    await checkpointManager.createCheckpoint("Validate 'Forgot Password' Link Visibility on Login Page", await loginPage.isLinksOnLoginPage('Forgot Password?'), true, "'Forgot Password' link should be visible on Login Page");
+    await checkpointManager.createCheckpoint("Validate iOS App Store Link Visibility on Login Page", await loginPage.isLinksOnLoginPage('iPhone'), true, "'iOS App Store' link should be visible on Login Page");
+    await checkpointManager.createCheckpoint("Validate Android App Store Link Visibility on Login Page", await loginPage.isLinksOnLoginPage('Android'), true, "'Android App Store' link should be visible on Login Page");
+    await checkpointManager.createCheckpoint("Validate Launguage Selection Dropdown Visibility on Login Page", await loginPage.isLaunguageSectingVisible(), true, "Language Selection Dropdown should be visible on Login Page");
+    await loginPage.clickOnLanguageSelector();
+    await checkpointManager.createCheckpoint("Validate English Language Option Visibility on Login Page", await loginPage.verifyLanguageOptionsVisible('English'), true, "'English' language option should be visible on Login Page");
+    await checkpointManager.createCheckpoint("Validate Español Language Option Visibility on Login Page", await loginPage.verifyLanguageOptionsVisible('Español'), true, "'Español' language option should be visible on Login Page");
+    await checkpointManager.assertAllCheckpoints();
+  
+  });
+ 
 });
