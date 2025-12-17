31bc8212b098f6628a5436ddffb367ddcfb417c6
Avinash Thummala <avinash.thummala@ucovyconnects.com>
2025-12-17 00:01:42 -0600

    Ligin Change for test in git hub actions

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,51 +1,11 @@
-// @ts-nocheck
-import { expect } from "@playwright/test";
-import { baseBest as test } from "../fixtures/baseFixture.js";
-import { LoginPage } from "../pages/LoginPage/LoginPage.js";
-import { RegisterPage } from "../pages/Registration/Registration.js";
-import { AdminLoginPage } from "../pages/LoginPage/AdminPage.js";
-import { CheckpointManager } from "../utils/CheckpointManager.js";
- 
-test.describe("Login Page Tests", () => {
- 
-  let email = "";
-  test("should show error message for invalid credentials @smoke @negative", async ({ authenticatedPage, testData }) => {
-    const { login, registration } = testData;
-    const loginPage = new LoginPage(authenticatedPage);
-    const registrationPage = new RegisterPage(authenticatedPage);
-    const checkpointManager = new CheckpointManager();
- 
-    await loginPage.enterUsername(login.InvalidUsername);
-    await loginPage.enterPassword(login.InvalidPassword);
-    await loginPage.clickShowPassword();
-    checkpointManager.createCheckpoint("Validating Password Visibility After Show Password Click", await loginPage.verifyPasswordVisible(login.InvalidPassword), false, "Password should be visible after first toggle");
-    await loginPage.clickShowPassword();
-    checkpointManager.createCheckpoint("Validating Password Visibility After Hide Password Click", await loginPage.verifyPasswordVisible(login.InvalidPassword), true, "Password should be not visible after second toggle");
-    await loginPage.clickLogin();
-    checkpointManager.assertAllCheckpoints();
-  });
+import { test, expect } from '@playwright/test';
 
-  test("Login with valid credentials @smoke @positive", async ({ authenticatedPage, testData }) => {
-    const { login } = testData;
-    const loginPage = new LoginPage(authenticatedPage);
-    await loginPage.login(login.ValidUsername, login.ValidPassword);
-    expect(await loginPage.verifyLoginSuccess()).toBeTruthy();
+test('test', async ({ page }) => {
+  await page.goto('https://ecommerce-playground.lambdatest.io/');
+  await expect(page.getByRole('heading', { name: 'Top categories close' })).toBeVisible();
 
-  });
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page.getByRole('heading', { name: 'Top categories close' })).toBeVisible();
 
-  test("Validate Links on Login Page @regression", async ({ authenticatedPage }) => {
-    const checkpointManager = new CheckpointManager();
-    const loginPage = new LoginPage(authenticatedPage);
-    await checkpointManager.createCheckpoint("Validate Logo Visibility on Login Page", await loginPage.isLogoVisibleOnLoginPage(), true, "Logo should be visible on Login Page");
-    await checkpointManager.createCheckpoint("Validate 'Forgot Password' Link Visibility on Login Page", await loginPage.isLinksOnLoginPage('Forgot Password?'), true, "'Forgot Password' link should be visible on Login Page");
-    await checkpointManager.createCheckpoint("Validate iOS App Store Link Visibility on Login Page", await loginPage.isLinksOnLoginPage('iPhone'), true, "'iOS App Store' link should be visible on Login Page");
-    await checkpointManager.createCheckpoint("Validate Android App Store Link Visibility on Login Page", await loginPage.isLinksOnLoginPage('Android'), true, "'Android App Store' link should be visible on Login Page");
-    await checkpointManager.createCheckpoint("Validate Launguage Selection Dropdown Visibility on Login Page", await loginPage.isLaunguageSectingVisible(), true, "Language Selection Dropdown should be visible on Login Page");
-    await loginPage.clickOnLanguageSelector();
-    await checkpointManager.createCheckpoint("Validate English Language Option Visibility on Login Page", await loginPage.verifyLanguageOptionsVisible('English'), true, "'English' language option should be visible on Login Page");
-    await checkpointManager.createCheckpoint("Validate Español Language Option Visibility on Login Page", await loginPage.verifyLanguageOptionsVisible('Español'), true, "'Español' language option should be visible on Login Page");
-    await checkpointManager.assertAllCheckpoints();
-  
-  });
- 
+  await page.getByRole('link', { name: 'Blog', exact: true }).click();
 });
